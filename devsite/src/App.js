d1e304362662322a835902d64f6956517ad0c187
Cameron Rosenberger <crosenberger17@gmail.com>
2021-08-22 16:32:06 -0700

    Refactored site into React

---
+++ devsite/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>div 1</div>
+      </div>
+    );
+  }
+}
+
+export default App;
